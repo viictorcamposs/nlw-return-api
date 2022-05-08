7f33a2a8f42348961daadcad4946616f7d0ef6fe
Victor Campos <victor.campos@escolaconquer.com.br>
2022-05-08 20:46:03 -0300

    build: setup for deploy

--- src/useCases/SubmitFeedbackUseCase.ts
+++ src/useCases/SubmitFeedbackUseCase.ts
@@ -34,6 +34,7 @@ export class SubmitFeedbackUseCase {
         '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
         `<p>Tipo do feedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
+        screenshot ? `<img src="${screenshot}" height="350" width="475" />` : null,
         '</div>',
       ].join('\n')
     })
